9a7df1985f7e0bf79c10105e4ffd786929cae39e
Baister <gustavobaister@gmail.com>
2025-01-14 19:31:47 -0300

    Numeros Primos

---
+++ Lista_De_Condicionais_e_Loops/NumerosPrimos/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Números Primos:
+Peça ao usuário um número e exiba se ele é primo ou não.*/
+Console.WriteLine("\nVerificador de Números Primos");
+Console.WriteLine("-----------------------------");
+
+Console.Write("Digite o número: ");
+int num = Convert.ToInt16(Console.ReadLine());
+
+if (num > 1 && num % 2 != 0)
+{
+
+    Console.WriteLine($"Esse número {num} é primo!");
+}
+if (num == 2)
+{
+    Console.WriteLine($"Esse número {num} é primo!");
+}
+Console.WriteLine();
