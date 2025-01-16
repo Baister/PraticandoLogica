a1d004e8a767b5e345ff0b1c70f7d42208fbf4e8
Baister <gustavobaister@gmail.com>
2025-01-16 18:29:17 -0300

    Exercicio Funcao Fatorial

---
+++ Lista_De_Condicionais_e_Loops/Fatorial/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Fatorial de um Número:
+Crie uma função que calcule o fatorial de um número recebido como parâmetro.
+*/
+
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("\nFATORIAL PROGRAM");
+Console.WriteLine("------------------\n");
+
+bool continuar = true;
+do {
+    try {
+        Console.Write("\nDigite um número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int fatorial (int num)
+        {
+            int multiplicando = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                
+                multiplicando *= i;
+            }
+            int fatorial = multiplicando;
+            return fatorial;
+        };
+
+        Console.WriteLine($"O fatorial de {num}! é igual a {fatorial(num)}.\n \n");
+        
+        Console.WriteLine("Se desejar parar digite [0], caso não, pressione qualquer tecla.");
+        string parar = Console.ReadLine();
+        if (parar == "0")
+            {
+                Console.WriteLine("\nFinalizando...\n");
+                continuar = false;
+            }
+    } catch (FormatException) {
+        Console.WriteLine("Favor inserir um valor válido!");
+    }
+    
+}while (continuar);
