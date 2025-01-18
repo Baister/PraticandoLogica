4040467cec4d3c848ed39c86ce37df81597bc847
Baister <gustavobaister@gmail.com>
2025-01-18 15:59:43 -0300

    Exercicio Conversor de Temperatura

---
+++ Lista_De_Condicionais_e_Loops/ConversorDeTemperatura/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Conversor de Temperatura:
+Crie uma função que converta graus Celsius para Fahrenheit.
+*/
+
+Console.WriteLine("\n\nCONVERSOR DE TEMPERATURA 3000");
+Console.WriteLine("-----------------------------");
+
+Console.Write("\nDigite a temperatura em graus celsius: ");
+double temperatura = ConversorDeTemperatura(Convert.ToDouble(Console.ReadLine()));
+
+Console.WriteLine($"\nConvertendo para Fahrenheit: {temperatura}ºF\n\n");
+
+double ConversorDeTemperatura(double temperatura)
+{
+    // convertendo para Fahrenheit
+
+    //Fórmula para fahrenheit é F= 1.8 * C + 32
+    double Fahrenheit = 1.8 * temperatura + 32;
+
+    return Fahrenheit;
+
+}
