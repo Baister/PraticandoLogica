89b7962e008b1e38a1565d2ad182e799f92b54cb
Baister <gustavobaister@gmail.com>
2025-01-18 16:35:24 -0300

    Exercicio Palindromo

---
+++ Lista_De_Condicionais_e_Loops/Palindromo/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Palíndromo:
+Escreva uma função que receba uma palavra e determine se ela é um palíndromo (lê-se da mesma forma de trás para frente).*/
+
+
+Console.WriteLine("\n\nPALÍNDROMON");
+Console.WriteLine("-------------\n\n");
+
+Console.Write("Digite uma palavra e a função irá verificar se ela é um palíndromo ou não: ");
+string? palavra = Palindromo(Console.ReadLine());
+
+Console.WriteLine(palavra);
+
+string Palindromo(string palavra)
+{
+    string palavraReversa = "";
+
+    foreach(char letra in palavra.Reverse())
+    {
+        /* Fiz com que a variável palavraReversa adicionasse os caracteres para formar a palavra reversa!
+        Console.Write(letra); */
+        palavraReversa += Convert.ToString(letra);
+    }
+    //Console.WriteLine(palavraReversa); //Testando
+    
+    if (palavra == palavraReversa)
+    {
+        string resultado = ($"\nWOW! ESSA PALAVRA É UM PALÍNDROMO!!! É IGUAL DE TRÁS PRA FRENTE.\nVeja só: {palavra}, {palavraReversa}\n\n");
+
+        return resultado;
+    }
+    else
+    {
+        string resultado = $"Elas não são palíndromos...\nAté porque elas são diferentes: {palavra} é diferente de {palavraReversa}\n\n";
+        return resultado;
+    };
+};
