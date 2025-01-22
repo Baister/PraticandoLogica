50dcdec57a9433811af1d4a1a52a05e763ec5f3a
Baister <gustavobaister@gmail.com>
2025-01-22 09:55:16 -0300

    Adicionando Exercício de String Reversa

---
+++ Lista_De_Condicionais_e_Loops/ReversoString/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Reverso de uma String:
+Solicite ao usuário uma palavra e exiba seu reverso*/
+
+Console.WriteLine("REVERSE STRING 3000");
+Console.WriteLine("-------------------");
+
+Console.Write("Digite uma palavra e você verá o inverso: ");
+string? palavra = Console.ReadLine();
+string palavraReverse = "";
+
+foreach(char letra in palavra.Reverse())
+{
+    palavraReverse += Convert.ToString(letra);
+}
+Console.WriteLine($"A palavra ao contrário fica: {palavraReverse}");
